850f105aa4e5cb687bfc46bbf365be9a45dfc145
xuhuipeng <326660884@qq.com>
2019-07-12 15:38:29 +0800

    放入静态数据

---
+++ src/components/layerMode.vue
@@ -0,0 +1,59 @@
+<template>
+	<el-container >
+        <el-header style="font-size: 20px;">安全事件详情</el-header>
+        <el-main>
+            <el-table
+            :data="tableData"
+            style="width: 100%">
+                <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="source"
+                    label="攻击源"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="target"
+                    label="攻击目标">
+                </el-table-column>
+                <el-table-column
+                    prop="number"
+                    label="次数">
+                </el-table-column>
+            </el-table>    
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          tableData: [{
+            date: '2019-07-11',
+            source:'中国联通',
+            target:'159.168.101.12',
+            number:'86039'
+          },{
+            date: '2019-07-11',
+            source:'中国联通',
+            target:'159.168.101.12',
+            number:'86039'
+          },{
+            date: '2019-07-11',
+            source:'中国联通',
+            target:'159.168.101.12',
+            number:'86039'
+          },{
+            date: '2019-07-11',
+            source:'中国联通',
+            target:'159.168.101.12',
+            number:'86039'
+          }]
+        }
+      }
+    }
+  </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,30 +9,28 @@
 				</div>
 				<div class="header-title"
 						 @click="routeJump">
-					<el-button type="text">监控系统</el-button>
+					<el-button type="text">昨日运行态势分析</el-button>
 				</div>
 				<div class="header-title"
 						 @click="routeJump">
-					<el-button type="text">监控系统</el-button>
+					<el-button type="text">安全指数</el-button>
 				</div>
 				<div class="header-title"
 						 @click="routeJump">
-					<el-button type="text">监控系统</el-button>
+					<el-button type="text">态势感知</el-button>
 				</div>
 			</div>
 			<div class="warp">
 				<div class="left-container">
-					<h1 class="title">昨天解析比例</h1>
+					<h2 class="title">昨天解析情况</h2>
 					<ve-ring :colors="colors"
 									 :legend-visible="false"
 									 height="300px"
-									 :data="chartData"
-									 :title="chartSettings"></ve-ring>
-					
+									 :data="yesterday"></ve-ring>
+					<h2 class="title">昨日服务器被攻击统计</h2>
 					<ve-pie :colors="colors"
 									:legend-visible="false"
-									:data="chartData"
-									:settings="chartSettings"></ve-pie>
+									:data="attacked"></ve-pie>
 					
 				</div>
 				<div class="middle-container">
@@ -41,43 +39,27 @@
 							 @openDialog="openDialog"></Map>
 				</div>
 				<div class="right-container">
-					<h1 class="title">昨天解析比例</h1>
+					<h2 class="title">攻击类型分析</h2>
 					<ve-radar :legend-visible="false"
-										:data="chartData"
 										height="300px"
 										style="top:70px"
-										:colors="colors"></ve-radar>
-					<h1 class="title">昨天解析比例</h1>
+										:colors="colors"
+										:data="a_type"></ve-radar>
+					<h2 class="title">安全事件</h2>
 					<ve-bar :legend-visible="false"
 									:colors="colors"
 									style="top:100px"
 									height="300px"
 									:grid="grid"
-									:data="chartData"></ve-bar>
+									:data="fourthChartData"></ve-bar>
 				</div>
 			</div>
 		</div>
 		<el-dialog :visible.sync="centerDialogVisible"
 							 class="dialog"
-							 width="30%"
+							 width="50%"
 							 center>
-			<div class="dialog-container">
-				<h1 class="title">详情</h1>
-				<ve-pie :colors="colors"
-								:legend-visible="false"
-								:grid="grid"
-								height="300px"
-								:data="chartData"></ve-pie>
-				<ve-line class="charts-container"
-								 :colors="colors"
-								 :legend-visible="false"
-								 :data="chartData"
-								 height="300px"
-								 :settings="chartSettings"></ve-line>
-			</div>
-			<span slot="footer"
-						class="dialog-footer">
-			</span>
+			<layerMode></layerMode>
 		</el-dialog>
 	</div>
 </template>
@@ -87,24 +69,17 @@ import { getList } from '@/api/map'
 import theme from '@/assets/json/halloween.project.json'
 import chalktheme from '@/assets/json/chalk.project.json'
 import waldenTheme from '@/assets/json/walden.project.json'
-// @ is an alias to /src
-// import chartRing from '@/components/charts/ring'
-// import chartPie from '@/components/charts/pie'
-// import chartHistogram from '@/components/charts/histogram'
-// import chartLiquidfill from '@/components/charts/liquidfill'
-// import chartLine from '@/components/charts/line'
 import Map from '@/components/map'
+import layerMode from '@/components/layerMode'
 
 export default {
 	name: 'home',
 	components: {
-		Map
+		Map,
+		layerMode
 	},
+	
 	data() {
-		this.title = {
-			text: '昨天解析比例',
-			show: true
-		}
 		this.colors = ['#2f4554', '#325299',
 			'#1A93F8', '#3E95CB']
 		this.xAxis = {
@@ -128,27 +103,54 @@ export default {
 		return {
 			mapInfoJson: {},
 			centerDialogVisible: false,
-			chartData: {
-				columns: ['日期', '访问用户', '下单用户', '下单率'],
+			//昨天情况
+			yesterday:{
+				columns: ['解析状态', '次数', '比例'],
 				rows: [
-					{ '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-					{ '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-					{ '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-					{ '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-					{ '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-					{ '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+					{'解析状态':'成功', '次数':'85460', '比例':'85.85%'},
+					{'解析状态':'失败', '次数':'171', '比例':'0.17%'},
+					{'解析状态':'拒接', '次数':'14560', '比例':'14.33%'}
+				]	
+			},
+			//被攻击
+			attacked:{
+				columns: ['被攻击目标', '次数'],
+				rows: [
+					{'被攻击目标':'天枢服务器群', '次数':'85460'},
+					{'被攻击目标':'天璇服务器群', '次数':'171'},
+					{'被攻击目标':'天玑服务器群', '次数':'4560'},
+					{'被攻击目标':'天权服务器群', '次数':'25460'},
+					{'被攻击目标':'玉衡服务器群', '次数':'3171'},
+					{'被攻击目标':'开阳服务器群', '次数':'60'},
+					{'被攻击目标':'摇光服务器群', '次数':'50'}
 				]
 			},
-			chartSettings:{
-				
-					text:'昨天解析比例',
-					show:true
-				
+			//攻击type
+			a_type:{
+				columns: ['ip','web协议','DDOS攻击','ARP攻击','洪水攻击','其他'],
+				rows: [
+					{'ip':'天枢服务器群', 'web协议':'25750', 'DDOS攻击':'2550', 'ARP攻击':'2550', '洪水攻击':'15750','其他':'100'},
+					{'ip':'天璇服务器群', 'web协议':'45030', 'DDOS攻击':'0', 'ARP攻击':'0', '洪水攻击':'0','其他':'121'},
+					{'ip':'天权服务器群', 'web协议':'2102', 'DDOS攻击':'30054', 'ARP攻击':'4025', '洪水攻击':'29510','其他':'150'},
+					{'ip':'摇光服务器群', 'web协议':'120', 'DDOS攻击':'18044', 'ARP攻击':'501', '洪水攻击':'24750','其他':'66'}
+				]
+			},
+			// 第四个图表数据，没想好名字
+			fourthChartData	:{
+				columns: ['name','已防护','已屏蔽','已忽略'],
+				rows: [
+					{'name':'天枢服务器群','已防护':'20100', '已屏蔽':'2750', '已忽略':'2550'},
+					{'name':'天璇服务器群','已防护':'20100', '已屏蔽':'2750', '已忽略':'2550'},
+					{'name':'天权服务器群','已防护':'1905', '已屏蔽':'7121', '已忽略':'8135'},
+					{'name':'摇光服务器群','已防护':'13156', '已屏蔽':'2102', '已忽略':'5468'},
+					{'name':'开阳服务器群','已防护':'15671', '已屏蔽':'9501', '已忽略':'18800'},
+				]
 			}
 		}
 	},
 	methods: {
-		openDialog() {
+		openDialog(marker) {
+			console.log(marker)
 			this.centerDialogVisible = true
 		},
 		routeJump() {
@@ -274,7 +276,7 @@ export default {
 
 	.dialog {
 		.dialog-container {
-			height: 600px;
+			
 			color: white;
 		}
 	}
